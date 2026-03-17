2b09c53b2a639dbc3d75e727822f12f1d7ff41f7
vikaskamankar-mgs <vikas.kamankar@multigenesys.com>
2026-03-17 17:32:02 +0530

    Add countdown loop with blast off message

---
+++ while_countdown.py
@@ -0,0 +1,12 @@
+counter = 10
+
+# Loop runs as long as counter is greater than or equal to 1
+while counter >= 1:
+    # Print the current value of counter
+    print(counter)
+    
+    # Decrease the counter by 1 in each iteration
+    counter -= 1
+
+# This will be printed after the loop ends
+print("Blast off!")
